6cd15dd7319f5e4aa6809c00f4194f8f7d8f4a1b
LeeBox <li_xiaoliang@foxmail.com>
2016-12-20 13:54:26 +0800

    done

--- function-sql.js
+++ function-sql.js
@@ -1,91 +1,182 @@
 var query = function () {
-    function _query () {
-      this.data = []
-      this._where = null
-      this._select = null
-      this._groupBy = null
-    }
-    _query.prototype.select = select
-    _query.prototype.from = from
-    _query.prototype.where = where
-    _query.prototype.orderBy = orderBy
-    _query.prototype.groupBy = groupBy
-    _query.prototype.having = having
-    _query.prototype.execute = execute
-    _query
-    return new _query()
+  function _query () {
+    this.data = null
+    this._where = []
+    this._select = null
+    this._groupBy = null
+    this._having = null
+    this._orderBy = null
+  }
+  _query.prototype.select = select
+  _query.prototype.from = from
+  _query.prototype.where = where
+  _query.prototype.orderBy = orderBy
+  _query.prototype.groupBy = groupBy
+  _query.prototype.having = having
+  _query.prototype.execute = execute
+  _query
+  return new _query()
 }
 
 function select (fn) {
+  if (this._select !== null) {
+    throw new Error('Duplicate SELECT')
+  }
   this._select = fn
   return this
 }
 
-function from (datasource) {
-  this.data = datasource
+function from (...datasource) {
+  if (this.data !== null) {
+    throw new Error('Duplicate FROM')
+  }
+  this.data = fulljoin(...datasource)
   return this
 }
 
-function where (fn) {
-  this._where = fn
+function where (...fn) {
+  this._where.push(fn)
   return this
 }
 
-function orderBy () {}
+function orderBy (fn) {
+  if (this._orderBy !== null) throw new Error('Duplicate ORDERBY')
+  this._orderBy = fn
+  return this
+}
 
 function groupBy (...fn) {
+  if (this._groupBy !== null) throw new Error('Duplicate GROUPBY')
   this._groupBy = fn
   return this
 }
 
-function having () {}
+function having (...fn) {
+  this._having = fn
+  return this
+}
 
 function execute () {
-  if (this._where) {
-    this.data = this.data.filter(p => this._where(p))
+  this.data = this.data || []
+  if (this._where.length > 0) {
+    for (let i = 0, l = this._where.length; i < l; i++) {
+      this.data = this.data.filter(p => {
+        return this._where[i].some(w => {
+          return w(p)
+        })
+      })
+    }
+  }
+  if (this._groupBy) {
+    var firstGroupby = this._groupBy.shift()
+    this.data = arrToObj(this.data, firstGroupby)
+    this._groupBy.forEach(groupby => {
+      findArrOnObj(this.data, groupby)
+    })
+    this.data = objToArr(this.data)
+    if (this._having) {
+      this.data = this.data.filter(item => {
+        return this._having.some(having => {
+          return having(item)
+        })
+      })
+    }
+  }
+  if (this._orderBy) {
+    this.data.sort(this._orderBy)
   }
   if (this._select) {
     this.data = this.data.map(p => this._select(p))
   }
-  if (this._groupBy) {
-    // console.log(Array.isArray(this._groupBy))
-    this._groupBy.forEach(groupby => {
-      this.data = this.data.
-    })
-  }
   return this.data
+}
+
+function fulljoin (...arr) {
+  var result = []
+  if (arr.length === 1) {
+    result = [...arr[0]]
+  } else {
+    for (let i = 0, l = arr[0].length; i < l; i++) {
+      for (let j = 0, ll = arr[1].length; j < ll; j++) {
+        result.push([arr[0][i], arr[1][j]])
+      }
+    }
+    for (let i = 2, l = arr.length; i < l; i++) {
+      let res = []
+      for (let j = 0, ll = result.length; j < ll; j++) {
+        for (let k = 0, lll = arr[i].length; k < lll; k++) {
+          res.push([...result[j], arr[i][k]])
+        }
+      }
+      result = res
+    }
+  }
+  return result
+}
+
+function findArrOnObj (obj, fn) {
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (isObject(obj[key])) {
+        findArrOnObj(obj[key], fn)
+      } else if (isArray(obj[key])) {
+        obj[key] = arrToObj(obj[key], fn)
+      }
+    }
+  }
+}
+
+function arrToObj (arr, fn) {
+  return arr.reduce((previous, current) => {
+    var name = fn(current)
+    if (!previous[name]) previous[name] = []
+    previous[name].push(current)
+    return previous
+  }, {})
 }
 
 function objToArr (obj) {
   var arr = []
   for (var key in obj) {
-    if (obj.hasOwnPorperty(key)) {
+    if (obj.hasOwnProperty(key)) {
       var _arr = []
-      _arr.push(key)
-      obj[key].forEach(n => {
-        if (toString.call(n) === '[object Object]') {
-          _arr.push(objToArr(n))
-        } else {
-          _arr.push(n)
-        }
-      })
+      if (key.match(/\d/g)) {
+        _arr.push(Number(key))
+      } else {
+        _arr.push(key)
+      }
+      if (isObject(obj[key])) {
+        _arr.push(objToArr(obj[key]))
+      } else {
+        _arr.push(obj[key])
+      }
+      arr.push(_arr)
     }
   }
+  return arr
 }
 
-var g = {
-  odd: [1,2,3,4,5],
-  even: [2,4,6,7]
+function isArray (arr) {
+  return Array.isArray(arr) || toString.call(arr) === '[object Array]'
 }
-var gg = {
-  odd: {
-    divisible: [1,2],
-    prime: [2,3]
-  },
-  even: {
-    prime: [1,23,3],
-    divisible: [1,2]
-  }
+
+function isObject (obj) {
+  return toString.call(obj) === '[object Object]'
 }
+
+// var g = {
+//   odd: [1, 2, 3, 4, 5],
+//   even: [2, 4, 6, 7]
+// }
+// var gg = {
+//   odd: {
+//     divisible: [1, 2],
+//     prime: [2, 3]
+//   },
+//   even: {
+//     prime: [1, 23, 3],
+//     divisible: [1, 2]
+//   }
+// }
 
 module.exports = exports = query
--- function-sql.test.js
+++ function-sql.test.js
@@ -93,14 +93,14 @@ describe('SQL tests', () => {
     }
 
     //SELECT * FROM numbers GROUP BY parity HAVING
-    // assert.deepEqual(query().select().from(numbers).groupBy(parity).having(odd).execute(), [["odd",[1,3,5,7,9]]]);
+    assert.deepEqual(query().select().from(numbers).groupBy(parity).having(odd).execute(), [["odd",[1,3,5,7,9]]]);
 
     function descendentCompare(number1, number2) {
       return number2 - number1;
     }
 
     //SELECT * FROM numbers ORDER BY value DESC
-  //  assert.deepEqual(query().select().from(numbers).orderBy(descendentCompare).execute(), [9,8,7,6,5,4,3,2,1]);
+   assert.deepEqual(query().select().from(numbers).orderBy(descendentCompare).execute(), [9,8,7,6,5,4,3,2,1]);
 
     function lessThan3(number) {
       return number < 3;
@@ -111,6 +111,134 @@ describe('SQL tests', () => {
     }
 
     //SELECT * FROM number WHERE number < 3 OR number > 4
-    // assert.deepEqual(query().select().from(numbers).where(lessThan3, greaterThan4).execute(),  [1,2,5,6,7,8,9]);
+    assert.deepEqual(query().select().from(numbers).where(lessThan3, greaterThan4).execute(),  [1,2,5,6,7,8,9]);
+  })
+
+  it('Frequency tests', function() {
+
+    var persons = [
+      ['Peter', 3],
+      ['Anna', 4],
+      ['Peter', 7],
+      ['Michael', 10]
+    ];
+
+    function nameGrouping(person) {
+      return person[0];
+    }
+
+    function sumValues(value) {
+      return [value[0], value[1].reduce(function(result, person) {
+        return result + person[1];
+      }, 0)];
+    }
+
+    function naturalCompare(value1, value2) {
+      if (value1 < value2) {
+        return -1;
+      } else if (value1 > value2) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+    //SELECT name, sum(value) FROM persons ORDER BY naturalCompare GROUP BY nameGrouping
+    assert.deepEqual(query().select(sumValues).from(persons).orderBy(naturalCompare).groupBy(nameGrouping).execute(),  [["Anna",4],["Michael",10],["Peter",10]]);
+
+    var numbers = [1, 2, 1, 3, 5, 6, 1, 2, 5, 6];
+
+    function id(value) {
+      return value;
+    }
+
+    function frequency(group) {
+      return { value: group[0], frequency: group[1].length };
+    }
+
+    //SELECT number, count(number) FROM numbers GROUP BY number
+    assert.deepEqual(query().select(frequency).from(numbers).groupBy(id).execute(), [{"value":1,"frequency":3},{"value":2,"frequency":2},{"value":3,"frequency":1},{"value":5,"frequency":2},{"value":6,"frequency":2}]);
+
+    function greatThan1(group) {
+      return group[1].length > 1;
+    }
+
+    function isPair(group) {
+      return group[0] % 2 === 0;
+    }
+
+    //SELECT number, count(number) FROM numbers GROUP BY number HAVING count(number) > 1 AND isPair(number)
+    assert.deepEqual(query().select(frequency).from(numbers).groupBy(id).having(greatThan1).having(isPair).execute(),  [{"value":2,"frequency":2},{"value":6,"frequency":2}]);
+  })
+
+  it('join tests', function() {
+    var teachers = [
+      {
+        teacherId: '1',
+        teacherName: 'Peter'
+      },
+      {
+        teacherId: '2',
+        teacherName: 'Anna'
+      }
+    ];
+
+
+    var students = [
+      {
+        studentName: 'Michael',
+        tutor: '1'
+      },
+      {
+        studentName: 'Rose',
+        tutor: '2'
+      }
+    ];
+
+    function teacherJoin(join) {
+      return join[0].teacherId === join[1].tutor;
+    }
+
+    function student(join) {
+      return {studentName: join[1].studentName, teacherName: join[0].teacherName};
+    }
+
+    //SELECT studentName, teacherName FROM teachers, students WHERE teachers.teacherId = students.tutor
+    assert.deepEqual(query().select(student).from(teachers, students).where(teacherJoin).execute(), [{"studentName":"Michael","teacherName":"Peter"},{"studentName":"Rose","teacherName":"Anna"}]);
+
+
+    var numbers1 = [1, 2];
+    var numbers2 = [4, 5];
+
+    assert.deepEqual(query().select().from(numbers1, numbers2).execute(), [[1,4],[1,5],[2,4],[2,5]]);
+
+    function tutor1(join) {
+      return join[1].tutor === "1";
+    }
+
+    //SELECT studentName, teacherName FROM teachers, students WHERE teachers.teacherId = students.tutor AND tutor = 1
+    assert.deepEqual(query().select(student).from(teachers, students).where(teacherJoin).where(tutor1).execute(), [{"studentName":"Michael","teacherName":"Peter"}]);
+  })
+
+  it('Duplication exception tests', function() {
+    function checkError(fn, duplicate) {
+      try {
+        fn();
+      } catch (e) {
+        assert.equal(e instanceof Error, true);
+        assert.equal(e.message, 'Duplicate ' + duplicate);
+      }
+    }
+
+
+    function id(value) {
+      return value;
+    }
+
+    checkError(function() { query().select().select().execute(); }, 'SELECT');
+    checkError(function() { query().select().from([]).select().execute(); }, 'SELECT');
+    checkError(function() { query().select().from([]).from([]).execute(); }, 'FROM');
+    checkError(function() { query().select().from([]).orderBy(id).orderBy(id).execute(); }, 'ORDERBY');
+    checkError(function() { query().select().groupBy(id).from([]).groupBy(id).execute(); }, 'GROUPBY');
+
   })
 })
